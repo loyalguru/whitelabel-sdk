2e9720d2c16b733be7e52a3cf68c86d1bcf8c812
Jose Aguera <jaguera@loyal.guru>
2025-11-14 13:57:33 +0100

    Handle Backend error

--- demo/config.js
+++ demo/config.js
@@ -1,7 +1,6 @@
   window.EMBED_SDK_CONFIG = {
     iframeOrigin: 'http://localhost:4200',
-    token: 'xxx-eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcl9pZCI6IjIwMTAiLCJjb21wYW55IjoicHVpZ19wcmUiLCJleHBpcmVzX2F0IjoiMjAyNS0xMS0zMFQxMjo0MzowMDowMFoifQ.vGqVYXp1LzVqYqyx6HYjBngQFuWoqESEDqrmxI92_6I',
-    //token: 'eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcl9pZCI6IjIwMSIsImNvbXBhbnkiOiJwdWlnX3ByZSIsImV4cGlyZXNfYXQiOiIyMDI1LTExLTMwVDEyOjQzOjAwOjAwWiJ9.6L1-ZbQCIzoTczQqmaOz1NSrb0v5PV82Yf6VO70Mm50',
+    token: 'eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcl9pZCI6IjIwMSIsImNvbXBhbnkiOiJwdWlnX3ByZSIsImV4cGlyZXNfYXQiOiIyMDI1LTExLTMwVDEyOjQzOjAwOjAwWiJ9.6L1-ZbQCIzoTczQqmaOz1NSrb0v5PV82Yf6VO70Mm50',
     locale: 'fr',
     module: '', 
     autoResize: false,
