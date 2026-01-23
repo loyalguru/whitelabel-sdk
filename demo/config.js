b0fac9f967eec50ba907f329f8e00fdeae01e382
Jose Aguera <jaguera@loyal.guru>
2026-01-23 15:36:44 +0100

    fixes

--- demo/config.js
+++ demo/config.js
@@ -1,6 +1,6 @@
   window.EMBED_SDK_CONFIG = {
     iframeOrigin: 'https://lg-app-palacio-dev.web.app/',
-    token: 'eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcl9pZCI6IjIwIiwiY29tcGFueSI6InBhbGFjaW9fZGV2IiwiZXhwaXJlc19hdCI6IjIwMjYtMTItMzBUMTI6NDM6MDA6MDBaIn0.6dOJZRHSsAQi4-fRF1dufgyHYubEkX9RH518RX4BZiQ',
+    token: 'eyJhbGciOiJIUzI1NiJ9.eyJjdXN0b21lcl9pZCI6IjIwIiwiY29tcGFueSI6InBhbGFjaW9fZGV2IiwiZXhwaXJlc19hdCI6IjIwMjUtMTItMzBUMTI6NDM6MDA6MDBaIn0.ItWIDWOgd_Oe3LMPFRalcTXBstF35X8mBVtThGEZlDM',
     locale: 'es',
     module: '', 
     autoResize: false,
